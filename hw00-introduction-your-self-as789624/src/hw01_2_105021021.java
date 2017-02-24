20daa9cd5fa53e41698224af1cd7311b401d0a2d
as789624 <105021021@live.asia.edu.tw>
2017-02-24 15:04:09 +0800

    hw submit

---
+++ hw00-introduction-your-self-as789624/src/hw01_2_105021021.java
@@ -0,0 +1,13 @@
+//2017 2/24
+//學生介紹
+//105021021 楊惟竣
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("楊惟竣");
+  System.out.println("105021021");
+  
+	}
+
+}
